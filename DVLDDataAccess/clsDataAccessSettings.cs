b0fde59ec62f31ffd957dc93ef2deebe6be730f3
Khaled Abu AlMajd <khaledabualmajd06@gmail.com>
2025-06-20 23:38:43 +0300

    Use app.config

--- DVLDDataAccess/clsDataAccessSettings.cs
+++ DVLDDataAccess/clsDataAccessSettings.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DVLDDataAccess
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD;User ID=sa;Password=123456";
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["DVLDConnectionString"].ConnectionString;
     }
 }
